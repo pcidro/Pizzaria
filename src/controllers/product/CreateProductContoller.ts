8233af09b427b1992b0e6ebc85e17f7beb0a471b
Paulo Cidro <paulocidro@gmail.com>
2026-07-12 21:07:38 -0300

    finalizando configuracao do cloudinary e postando produto com imagem

--- src/controllers/product/CreateProductContoller.ts
+++ src/controllers/product/CreateProductContoller.ts
@@ -4,16 +4,21 @@ import { CreateProductService } from "../../services/products/CreateProductServi
 
 export class CreateProductController {
   async handle(req: Request, res: Response) {
-    const { name, price, description, banner, category_id } = req.body;
-    if (!name || !price || !description || !banner || !category_id) {
+    const { name, price, description, category_id } = req.body;
+    if (!req.file) {
+      throw new Error("A imagem do produto é necessária");
+    }
+    if (!name || !price || !description || !category_id) {
       res.status(400).json({ error: "Todos os campos são necessários!" });
     }
     const createProduct = new CreateProductService();
+
     const product = await createProduct.execute({
       name,
       price,
       description,
-      banner,
+      imageBuffer: req.file.buffer,
+      imageName: req.file.originalname,
       category_id,
     });
     res.status(201).json(product);
